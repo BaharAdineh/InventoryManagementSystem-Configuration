2b51965e669da4aae5be7616f1540087e596dd10
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-04-09 13:04:54 +0200

    Initial commit

---
+++ src/main/java/com/challenge/inventorymanagementsystemconfiguration/InventoryManagementSystemConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.inventorymanagementsystemconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSystemConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSystemConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/inventorymanagementsystemconfiguration/InventoryManagementSystemConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.inventorymanagementsystemconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
