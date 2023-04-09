136d163423652d8a60a8b1fc4780155c5555b0ea
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-04-09 13:18:16 +0200

    Initial commit

--- src/test/java/com/challenge/inventorymanagementsystemconfiguration/InventoryManagementSystemConfigurationApplicationTests.java
+++ src/test/java/com/challenge/inventorymanagementsystemconfiguration/InventoryManagementSystemConfigurationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InventoryManagementSystemConfigurationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
